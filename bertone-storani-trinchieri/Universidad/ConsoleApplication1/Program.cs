de757e205fdf781fdc1f6fdddb431f0906dcf4aa
gianstorani <gianstorani9@gmail.com>
2017-04-11 08:59:46 -0300

    Overload y Singleton

--- bertone-storani-trinchieri/Universidad/ConsoleApplication1/Program.cs
+++ bertone-storani-trinchieri/Universidad/ConsoleApplication1/Program.cs
@@ -25,33 +25,45 @@ namespace ConsoleApplication2
 
     public enum DepartamentosAcademicos { Santiago, Rafaela, Olivos, Jujuy}
 
-    public class universidad
-    {
-        public List<DepartamentosAcademicos> Departamentos { get; set; }
-    }
+    public class Universidad
+    {       
+        public DepartamentosAcademicos Departamento { get; set; }
 
+        public Universidad(DepartamentosAcademicos d) { this.Departamento = d; }
+
+        private static List<Universidad> departamentos = new List<Universidad>();
+
+        public static Universidad AgregarDepartamento(DepartamentosAcademicos d)
+        {
+            foreach(Universidad u in departamentos)
+            {
+                if (u.Departamento == d )return u;
+            }
+            Universidad univ = new Universidad(d);
+            departamentos.Add(univ);
+
+            return univ;
+        }
+    }
+    
     public class Persona
     {
         public Direccion DireccionPersona { get; set; }
         public string Nombre { get; set; }
         public int Edad { get; set; }
-
     }
 
     public class Año
     {
         public List<Materia> Materias { get; set; }
-
         public Año()
         {
             this.Materias = new List<Materia>();
         }
-
     }
     
     public class Carrera
     {
-
         public string JefeCatedra { get; set; }
         public List<Año> MateriasAño { get; set; }
         public int AñosCursado { get; set; }
@@ -96,7 +108,6 @@ namespace ConsoleApplication2
     public class Materia
     {
         public string Nombre { get; set; }
-        public string Condicion { get; set; }
         public string Horarios { get; set; }
         public List<Materia> CorrelativasFuertes { get; set; }
         public List<Materia> CorrelativasDebiles { get; set; }
@@ -115,12 +126,37 @@ namespace ConsoleApplication2
     {
         public string Carrera { get; set; }
         public List<Materia> MateriasCursadas { get; set; }
+        public List<Materia> MateriasAprobadas { get; set; }
+
+        public string Condicion { get; set; }
+
 
         public Alumno()
         {
             this.MateriasCursadas = new List<Materia>();
         }
 
+        public Alumno(String nombre):this() 
+        {
+            this.Nombre = nombre;
+        }
+
+        public void AgregarMateria(Materia materia)
+        {
+            this.MateriasCursadas.Add(materia);
+        }
+
+        public void AgregarMateria(Materia materia, bool condicion)
+        {
+            if (condicion)
+            {
+                this.MateriasAprobadas.Add(materia);
+            }
+            else
+            {
+                this.MateriasCursadas.Add(materia);
+            }
+        }
     }
 
     public class Profesor : Persona
--- bertone-storani-trinchieri/Universidad/UnitTestProject1/CarreraTest.cs
+++ bertone-storani-trinchieri/Universidad/UnitTestProject1/CarreraTest.cs
@@ -15,21 +15,33 @@ namespace UnitTestProject1
             Carrera carrera = new Carrera();
             Materia M1 = new Materia();
             Año primero = new Año();
-
-            
             var alumno1 = new Alumno() { Nombre = "AL1" };
             var alumno2 = new Alumno() { Nombre = "AL2" };
-
             M1.Alumnos.Add(alumno1);
             M1.Alumnos.Add(alumno2);
-            
             primero.Materias.Add(M1);
             carrera.MateriasAño.Add(primero);
-            
+      
             //act
             var total = carrera.TotalAlumnos();
             //assert
             Assert.AreEqual(2, total);
+        }
+    }
+
+    public class UniversidadesTest
+    {
+        public void DeberiaAgregarUniversidad()
+        {
+            //arrange
+
+            DepartamentosAcademicos d = DepartamentosAcademicos.Rafaela;
+            Universidad universidad = new Universidad(d);
+
+            //act
+            Universidad.AgregarDepartamento(d);
+            //assert
+            Assert.AreEqual(1,)
         }
     }
 }
